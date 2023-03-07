b001a53cab0a2a7a220db3aa8c9990c384c17297
shukri74 <shukri.mohamed@mail.com>
2023-03-07 00:47:58 +0000

    package-lock.json added

--- index.js
+++ index.js
@@ -1,21 +1,80 @@
-const fs = require("fs");
-const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Who are the contributors of this project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How to install the project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How to use the project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license do you use?",
+    choices: [
+      {
+        name: "MIT License",
+        value: "MIT",
+      },
+      {
+        name: "Apache License 2.0",
+        value: "Apache-2.0",
+      },
+      {
+        name: "GPL 3.0 License",
+        value: "GPL-3.0",
+      },
+      {
+        name: "No License",
+        value: "None",
+      },
+    ],
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
 ];
-
 // function to write README file
 function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.log(err) : console.log("successfully generated")
+    );
+  }
+  
+  // function to initialize program
+  function init() {
+    inquirer.prompt(questions).then((responses) => {
+      console.log(responses);
+      const answers = generateMarkdown(responses);
+      writeToFile("README.md", answers, (err) =>
+        err ? console.log(err) : console.log("made it")
+      );
+    });
+  }
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,55 @@
-// function to generate markdown for README
+function renderLicenseBadge(license) {
+  let badgeURL = '';
+
+  switch (license) {
+    case 'MIT':
+      badgeURL = 'https://img.shields.io/badge/License-MIT-yellow.svg';
+      break;
+    case 'Apache 2.0':
+      badgeURL = 'https://img.shields.io/badge/License-Apache%202.0-blue.svg';
+      break;
+    case 'GPL 3.0':
+      badgeURL = 'https://img.shields.io/badge/License-GPLv3-blue.svg';
+      break;
+    default:
+      return '';
+  }
+
+  return `[![License](${badgeURL})](https://opensource.org/licenses/${license})\n\n`;
+}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+${tableOfContents(data)}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+${questionsSection(data)}
 `;
 }
-
-module.exports = generateMarkdown;
